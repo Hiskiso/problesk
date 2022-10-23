64eda8dfa03bfe40825a38ddb44874e2244328af
Hiskiso <roma123qq@gmail.com>
2022-10-23 22:49:38 +0300

    set real delays

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ let red = document.createElement("div")
 let blue = document.createElement("div")
 const isMobile = !/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-let modes = [[100, 100], [300, 300], [700,700], [100, 300]]
-let mode = 0
 
 document.body.appendChild(red)
 document.body.appendChild(blue)
@@ -14,8 +12,8 @@ if (isMobile) {
     document.body.style.display = "flex"
     document.body.style.flexDirection = "row"
     document.body.style.justifyContent = "flex-start"
-
 }
+
 document.body.style.background = "black"
 
 red.style.height = isMobile ? "100vh" : "50vh"
@@ -26,36 +24,26 @@ blue.style.height = isMobile ? "100vh" : "50vh"
 blue.style.width = isMobile ? "100vw" : "100vw"
 blue.style.background = "blue"
 
-
-let interval = setInterval(async()=>{
-    await delay(modes[mode][0])
-    red.style.background = ""
-    await delay(modes[mode][0])
-    red.style.background = "red"
-    await delay(modes[mode][1])
+let Mdelay = 50
+let count = 2 
+setInterval(async()=>{
     blue.style.background = ""
-    await delay(modes[mode][1])
-    blue.style.background = "blue"
-    }, (modes[mode][0]*2)+(modes[mode][1]*2))   
+    
 
-    red.addEventListener("click", ()=>{
-        clearInterval(interval)
-
-        mode += 1
-        if(mode >= modes.length){
-            mode = 0
-        }
-        console.log(modes[mode]) 
-        interval = setInterval(async()=>{
-            await delay(modes[mode][0])
-            red.style.background = ""
-            await delay(modes[mode][0])
-            red.style.background = "red"
-            await delay(modes[mode][1])
-            blue.style.background = ""
-            await delay(modes[mode][1])
-            blue.style.background = "blue"
-            }, (modes[mode][0]*2)+(modes[mode][1]*2))  
-     })
-        
+    for (let i = 0; i < count; i++) {
+        await delay(Mdelay)
+        red.style.background = "red"  
+        await delay(Mdelay)
+        red.style.background = ""
+       
+    }
+    red.style.background = ""
+    for (let i = 0; i < count; i++) {
+        await delay(Mdelay)
+        blue.style.background = "blue"  
+        await delay(Mdelay)
+        blue.style.background = ""
+    }
+  
    
+}, ((Mdelay * 2) * (count + 1)) + ((Mdelay * 2) * (count + 1)))
